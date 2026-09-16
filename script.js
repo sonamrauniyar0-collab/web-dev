1d7d3b1efab4a20f98c68702b00ef2cfa71284e5
Sonam <sonurauniyar2062@gmail.com>
2026-09-16 11:30:47 +0545

    week-7-class-2

--- script.js
+++ script.js
@@ -1,10 +1,78 @@
-console.log ("External script loaded")
-const x="sonam"
-console.log(x);
-const a=5;
-const b=6;
-const sum=a+b;
-console.log("sum",sum);
-console.log("Product:multiply");
-const multiply=a*b;
-console.log("Product",multiply);
+// console.log("External script loaded successfully!");
+// const x="Sonam"; 
+// console.log(x);
+// const a = 5;
+// const b = 10;
+// const sum = a + b;
+// console.log("sum:",sum);
+// const multiply = a * b;
+// console.log("Product:",multiply);
+// const remainder = a % b;
+// console.log("Remainder:",remainder);
+// //if x=7,y=8,z=9. Find if they are even number or odd number
+// const p = 7;
+// const q = 8;
+// const r = 9;
+
+// if (p % 2 == 0) {
+//     console.log(p + " is an even number");
+// } else {
+//     console.log(p + " is an odd number");
+// }
+
+// if (q % 2 == 0) {
+//     console.log(q + " is an even number");
+// } else {
+//     console.log(q + " is an odd number");
+// }
+
+// if (r % 2 == 0) {
+//     console.log(r + " is an even number");
+// } else {
+//     console.log(r + " is an odd number");
+// }
+
+let num = 5
+if (num === 5) {
+    console.log("number");
+}else if (num == 5) {
+    console.log("string");
+}
+
+function displayname(name)
+{
+    console.log("My name is",name);
+}
+displayname("Sonam");
+
+// create a function for sum, product, remainder of two numbers
+function sum(a,b){
+    console.log("The sum is:",a+b);
+}
+sum(4,10);
+
+function product(a,b){
+    console.log("The product is:",a*b);
+}
+product(4,10);
+
+function remainder(a,b){
+    console.log("The remainder is:",a%b);
+}
+remainder(4,10);    
+
+//create a function to check if a number is even or odd
+
+// let a = 5;
+let a = prompt("Enter any number to check odd or even");
+let result = checkEvenOdd(a);
+if (result == 0) {
+    // console.log(a + " is an even number");
+    alert(a + " is an even number");
+} else {
+    // console.log(a + " is an odd number");
+    alert(a + " is an odd number");
+}
+function checkEvenOdd(num){
+    return num % 2;
+}
